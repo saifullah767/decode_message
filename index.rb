c1470c6e95633b3d3821801b40dacdaf47f0a419
Ronald <rjskinnerc@unal.edu.co>
2022-07-13 15:14:39 -0500

    Add the decode method

--- index.rb
+++ index.rb
@@ -1,3 +1,14 @@
-def decode_char("")
-    puts "Bacon plz."
-  end
+def decode(string)
+  words = string.split('   ');
+  print(words);
+end
+
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...');
+
+  # given a string, with words searated by three spaces -  .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...
+  #   split the string and get the words  [.-,   -... --- -..-,   ..-. ..- .-.. .-..,   --- ..-.,   .-. ..- -... .. . ...]
+  #     for every word, e have to split and get every char [-..., ---, -..-]
+  #       for every char we have to get the letter corresponding to the morse code decode_char(char)
+  #         return the letter in uppercase return A, B, C, D
+  #       return the word in uppercase ABDC
+  #     return the full sentence RD REJLJK ELKJSB
